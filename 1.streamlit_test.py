8511bddbdf1d0905769c8b762f779061ad634883
최민서 <alstjchlrh13@egmail.com>
2025-03-12 12:04:13 +0900

    streamlit test code

---
+++ 1.streamlit_test.py
@@ -0,0 +1,8 @@
+# streamlit 실행방법
+# streamlit run 1. test.py
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ai_streamlit_ex
+openai와 streamlit 활용한 서비스 개발
+
+# 설치 라이브러리
+```
+pip install openai
+pip install streamlit
+```
